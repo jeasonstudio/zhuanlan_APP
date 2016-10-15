fc727f30c631c01042a926bb13f31966d5fc29f6
赵吉彤 <zjtong3576@sina.com>
2016-10-16 00:05:08 +0800

    add

--- main.js
+++ main.js
@@ -1,20 +1,76 @@
 'use strict';
 
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+// var app = require('app');
+// var BrowserWindow = require('browser-window');
 
 
+// var mainWindow = null;
+
+
+// app.on('ready', function() {
+//     mainWindow = new BrowserWindow({
+//         frame: false,
+//         height: 550,
+//         resizable: false,
+//         width: 900
+//     });
+
+//     mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+// });
+
+// const electron = require('electron');
+// 控制应用生命周期的模块。
+const app = require('app');
+// 创建原生浏览器窗口的模块。
+const BrowserWindow = require('browser-window');
+
+// 保持一个对于 window 对象的全局引用，如果你不这样做，
+// 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
 var mainWindow = null;
 
-
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        frame: false,
-        height: 550,
-        resizable: false,
-        width: 900
+function createWindow() {
+  // 创建浏览器窗口。
+  mainWindow = new BrowserWindow({
+    //   frame: false,  //无菜单栏
+      resizable: false,
+      width: 900,
+      height: 550
     });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+  // 加载应用的 index.html。
+  mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+  // 启用开发工具。
+  mainWindow.webContents.openDevTools();
+
+  // 当 window 被关闭，这个事件会被触发。
+  mainWindow.on('closed', function() {
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 与此同时，你应该删除相应的元素。
+    mainWindow = null;
+  });
+}
+
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.on('ready', createWindow);
+
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', function() {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  // 在 macOS 上，当点击 dock 图标并且该应用没有打开的窗口时，
+  // 绝大部分应用会重新创建一个窗口。
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
